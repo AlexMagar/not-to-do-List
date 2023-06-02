3b396ff05e397dba94a5fb99bf00a880d3049f36
AlexMagar <lx.mgr07@gmail.com>
2023-06-02 22:39:15 +1000

    added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Title } from './Components/Title';
+import { TopForm } from './Components/TopForm';
+import { BottomTable } from './Components/BottomTable';
+import { TotalHrsAllocated } from './Components/TotalHrsAllocated';
+import { EntryTaskList } from './Components/EntryTaskList';
+import { BadTaskList } from './Components/BadTaskList';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container-fluid wrapper">
+      <Title/>
+      <TopForm/>
+      <BottomTable/>
+      <EntryTaskList/>
+      <BadTaskList/>
+      <TotalHrsAllocated/>
     </div>
   );
 }
